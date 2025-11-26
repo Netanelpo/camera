db8a318309e35c1a1edf2f4df16ee415e6aece66
Netanel <niko0355@gmail.com>
2025-11-26 12:58:10 +0200

    first test

--- tests/camera_test.py
+++ tests/camera_test.py
@@ -1,6 +1,10 @@
-from camera import Camera
+from camera.camera import Camera
 
 
 def test_camera():
     camera = Camera()
-    assert camera._is_open
+    assert not camera._is_open
+    camera.open()
+    assert  camera.is_open
+    camera.close()
+    assert not camera.is_open
