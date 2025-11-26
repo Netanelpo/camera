cb75894df71eb00a281bae255d1036f063c7f0ea
Netanel <niko0355@gmail.com>
2025-11-26 13:14:24 +0200

    first test

--- tests/camera_test.py
+++ tests/camera_test.py
@@ -1,10 +1,26 @@
+import numpy as np
+import pytest
+
 from camera.camera import Camera
 
 
-def test_camera():
+def test_close():
     camera = Camera()
     assert not camera._is_open
     camera.open()
-    assert  camera.is_open
+    assert camera.is_open
     camera.close()
     assert not camera.is_open
+
+
+def test_image_closed():
+    camera = Camera()
+    with pytest.raises(RuntimeError):
+        next(camera.capture_image())
+
+def test_image_opened():
+    camera = Camera()
+    camera.open()
+    frame = next(camera.capture_image())
+    assert frame.shape == (1024, 1280, 1)
+    assert frame.dtype == np.uint16
